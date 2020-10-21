2494d037c1a949ea002609988c2e7eb2945decbf
wang <1141067260@qq.com>
2020-10-21 09:20:20 +0800

    测试

--- src/test/java8Lambda/stream/stream_1.java
+++ src/test/java8Lambda/stream/stream_1.java
@@ -12,6 +12,13 @@ import java.util.stream.Stream;
  * @description : 构造流的几种常见方法
  */
 public class stream_1 {
+    /**
+     * Stream是Java8的一大亮点，是对容器对象功能的增强，它专注于对容器对象进行各种非常便利、高效的 聚合操作（aggregate operation）或者大批量数据操作。
+     * Stream API借助于同样新出现的Lambda表达式，极大的提高编程效率和程序可读性。
+     * 同时，它提供串行和并行两种模式进行汇聚操作，并发模式能够充分利用多核处理器的优势，使用fork/join并行方式来拆分任务和加速处理过程。
+     * 所以说，Java8中首次出现的 java.util.stream是一个函数式语言+多核时代综合影响的产物。
+     * @param args
+     */
     public static void main(String[] args) {
         //1、Individual values
         Stream stream = Stream.of("a", "b", "c");
